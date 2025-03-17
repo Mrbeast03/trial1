b5b37820e18ea8dd3249a98b621a1c24089e2983
Mrbeast03 <ganeshkalyankar01@gmail.com>
2025-03-17 17:12:18 +0530

    fix port

--- abc/app.py
+++ abc/app.py
@@ -1213,7 +1213,7 @@ def fetch_today_records():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
 
 
 
